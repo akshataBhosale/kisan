00e71d045278e553f833c1fd10cf5a056ba2ab92
sohelpathan6411 <sohelpathan6411@gmail.com>
2021-05-12 18:21:21 +0530

    login with otp, profile create, update, getting user data

---
+++ lib/Models/UserDataParser.dart
@@ -0,0 +1,17 @@
+class ProfileParser {
+  String customerID,
+      customerAuthType,
+      customerFullname,
+      customerEmail,
+      customerMobileno,
+      customerFCM;
+
+  ProfileParser(
+    this.customerID,
+    this.customerAuthType,
+    this.customerFullname,
+    this.customerEmail,
+    this.customerMobileno,
+    this.customerFCM,
+  );
+}
---
+++ lib/Models/UserProfileParser.dart
@@ -0,0 +1,16 @@
+class UserDataParser {
+  String email,
+      username,
+      first_name,
+      last_name;
+
+  UserProfileParser  user_profile;
+
+  UserDataParser(
+    this.email,
+    this.username,
+    this.first_name,
+    this.last_name,
+    this.user_profile
+  );
+}
